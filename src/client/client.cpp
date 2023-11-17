48ef30f429ab01b96e3fddd1f6bf63f84e8a8e38
José Pereira <jose.a.pereira03@gmail.com>
2023-11-17 14:30:54 +0000

    Establish connection with tejo

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -6,7 +6,8 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdlib.h>
-#define PORT "58001"
+#include <iostream>
+#define PORT "58011"
 
 int fd, errcode, newfd;
 ssize_t n;
@@ -16,27 +17,39 @@ struct sockaddr_in addr;
 char buffer[128];
 
 int main() {
-    fd = socket(AF_INET, SOCK_STREAM, 0); // TCP socket, AF_INET = IPv4 abd SOCK_STREAM = TCP
+    fd = socket(AF_INET, SOCK_DGRAM, 0); // TCP socket, AF_INET = IPv4 abd SOCK_STREAM = TCP
     if (fd == -1) exit(1);
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_socktype = SOCK_DGRAM;
 
     errcode = getaddrinfo("tejo.tecnico.ulisboa.pt", PORT, &hints, &res);
     if (errcode != 0) exit(1);
 
-    errcode = connect(fd, res->ai_addr, res->ai_addrlen);
-    if (errcode == -1) exit(1);
-
-    n = write(fd, "Hdasdello!\n", 7);
+    /* n = sendto(fd, "LST\n", 4, 0, res->ai_addr, res->ai_addrlen);
     if (n == -1) exit(1);
 
-    n = read(fd, buffer, 128);
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr *) &addr, &addrlen);
     if (n == -1) exit(1);
 
-    write(1, "echo: ", 6);
-    write(1, buffer, n);
+    write(1, "received: ", 10);
+    write(1, buffer, n); */
+
+    while (!std::cin.eof()) {
+        std::cin.getline(buffer, 128);
+        buffer[strlen(buffer)] = '\n';
+        n = sendto(fd, buffer, strlen(buffer), 0, res->ai_addr, res->ai_addrlen);
+        if (n == -1) exit(1);
+
+        addrlen = sizeof(addr);
+        n = recvfrom(fd, buffer, 128, 0, (struct sockaddr *) &addr, &addrlen);
+        if (n == -1) exit(1);
+
+        write(1, "received: ", 10);
+        write(1, buffer, n);
+    }
 
     freeaddrinfo(res);
     close(fd);
