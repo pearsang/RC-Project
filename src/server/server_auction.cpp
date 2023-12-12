e6dce85a7fb74b701022950998077d919ccbf746
simaosanguinho <simao.sanguinho@gmail.com>
2023-12-12 22:01:13 +0000

    [server] validation of open auction - compile error

--- src/server/server_auction.cpp
+++ src/server/server_auction.cpp
@@ -1,7 +1,5 @@
 #include "server_auction.hpp"
 
-
-
 void validateOpenAuctionArgs(std::string userID, std::string password,
                              std::string auctionName, uint32_t startValue,
                              uint32_t timeActive, std::string assetFilename,
--- src/server/server_auction.hpp
+++ src/server/server_auction.hpp
@@ -13,15 +13,14 @@
 class AuctionManager {
 public:
   uint32_t openAuction(std::string userID, std::string auctionName,
-                              uint32_t startValue, uint32_t timeActive,
-                              std::string assetFilename);
+                       uint32_t startValue, uint32_t timeActive,
+                       std::string assetFilename);
 
   // constructor
   AuctionManager() = default;
   // destructor
   ~AuctionManager() = default;
 };
-
 
 void validateOpenAuctionArgs(std::string userID, std::string password,
                              std::string auctionName, uint32_t startValue,
